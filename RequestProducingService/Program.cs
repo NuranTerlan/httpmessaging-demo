a00312d5e9996c072ba8824e846d051eec3f875e
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-06-09 18:43:22 +0400

    user-defined messages requesting pipeline

---
+++ RequestProducingService/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RequestProducingService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Request-Producer service is started..\n");
+            while (true)
+            {
+                Console.Write("data: ");
+                string postedData = Console.ReadLine().PadRight(50, ' ') + DateTime.UtcNow.ToLocalTime();
+                HttpHelpers.SendPostRequest(postedData, "1155");
+            }
+        }
+    }
+}
