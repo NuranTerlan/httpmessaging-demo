4eaa3c49edd00c40d298aa818ef472717497fffa
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-06-09 18:43:03 +0400

    real-time requests listening pipeline

---
+++ HttpListeningService/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HttpListeningService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Http-Listener service is started..\n");
+
+            ListenerHelpers.ListenPorts(new string[] {"1155"});
+            Console.ReadLine();
+        }
+    }
+}
